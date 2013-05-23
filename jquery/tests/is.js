5d68e5b2c492de481a9c23f88870e3d42b57f00c
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2013-05-23 19:53:07 +0000

    Add tests for zip codes

--- jquery/tests/is.js
+++ jquery/tests/is.js
@@ -18,10 +18,86 @@ describe('Works when the value is as html', function() {
 
 describe("Regexes", function() {
 	describe("Zip codes for countries", function() {
+		it("Australia - format: ####", function() {
+			$.is('1122', 'zip:au').should.be.true;
+		});
+
+		it("Austria - format: ####", function() {
+			$.is('1122', 'zip:at').should.be.true;
+		});
+
+		it("Belgium - format: ####", function() {
+			$.is('1122', 'zip:be').should.be.true;
+		});
+
+		it("Brazil - format: #####-###", function() {
+			$.is('25251-121', 'zip:br').should.be.true;
+		});
+
+		it("Canada - format: A#A #A#", function() {
+			$.is('K2A 7B7', 'zip:ca').should.be.true;
+		});
+
+		it("Danmark - format: ### (or) ####", function() {
+			$.is('123', 'zip:dk').should.be.true;
+			$.is('1234', 'zip:dk').should.be.true;
+		});
+
+		it("Germany - format: #####", function() {
+			$.is('12345', 'zip:de').should.be.true;
+		});
+
+		it("Hungry - format: ####", function() {
+			$.is('1234', 'zip:hu').should.be.true;
+		});
+
 		it("Iceland - format: ###", function() {
-			$.is('12', 'zip:is').should.be.false;
 			$.is('112', 'zip:is').should.be.true;
-			$.is('1122', 'zip:is').should.be.false;
+		});
+
+		it("Italy - format: #####", function() {
+			$.is('12345', 'zip:it').should.be.true;
+		});
+
+		it("Japan - format: ###-####", function() {
+			$.is('123-1234', 'zip:jp').should.be.true;
+		});
+
+		it("Luxembourg - format: L-####", function() {
+			$.is('L-1234', 'zip:lu').should.be.true;
+		});
+
+		it("Nertherland - format: 1### AA", function() {
+			$.is('1234 aG', 'zip:nl').should.be.true;
+			$.is('1234AA', 'zip:nl').should.be.true;
+		});
+
+		it("Polland - format: ##-###", function() {
+			$.is('12-123', 'zip:pl').should.be.true;
+		});
+
+		it("Spain - format: [01-50]###", function() {
+			$.is('01123', 'zip:es').should.be.true;
+			$.is('50123', 'zip:es').should.be.true;
+		});
+
+		it("Sweden - format: ### ##", function() {
+			$.is('12312', 'zip:se').should.be.true;
+			$.is('123 12', 'zip:se').should.be.true;
+		});
+
+		it("United Kingdom - format: AA#A #AA (or) A#A #AA (or) A# #AA (or) A## #AA (or) AA# #AA (or) AA## #AA", function() {
+			$.is('gj5g 5hh', 'zip:gb').should.be.true;
+			$.is('h7u 5hh', 'zip:gb').should.be.true;
+			$.is('h7 5hh', 'zip:gb').should.be.true;
+			$.is('h76 5hh', 'zip:gb').should.be.true;
+			$.is('hf6 5hh', 'zip:gb').should.be.true;
+			$.is('hf63 5hh', 'zip:gb').should.be.true;
+		});
+
+		it("United States of America - format: ##### (or) #####-####", function() {
+			$.is('12135', 'zip:us').should.be.true;
+			$.is('12135-4342', 'zip:us').should.be.true;
 		});
 	});
 });
