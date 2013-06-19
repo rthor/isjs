3c68a2a4485c60c8e789474b45f7bb70a4fd545f
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2013-06-19 22:42:07 +0000

    Abstract deep functionality

--- vanilla/src/intro.js
+++ vanilla/src/intro.js
@@ -1,2 +1,5 @@
 window.is = function ( value, expression ) {
 	if (typeof expression === undefined) return false;
+
+	// Declare and initiate variables
+	var deep = false;
--- vanilla/src/outro.js
+++ vanilla/src/outro.js
@@ -1,16 +1,4 @@
 
-    var deep = false;
-
-    // If expression is deep
-    if (
-        typeof expression === 'string' &&
-        expression.indexOf(':') !== -1
-    ) {
-        expression = expression.match( /(\w+)(?:\:)(\w+)/ );
-        deep = expression[2];
-        expression = expression[1];
-    }
-
     if (regex.hasOwnProperty(expression)) {
         return test(value, deep ? regex[expression][deep] : regex[expression]);
     }
